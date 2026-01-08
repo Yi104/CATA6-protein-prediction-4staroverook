dc32c39becf6e0aed7e18c0153e98a9927b608f6
Yi_104 <yijin@brandeis.edu>
2026-01-07 17:06:14 -0800

    propagation add parents, and root terms

--- src/ontology/propagation.py
+++ src/ontology/propagation.py
@@ -1,9 +1,26 @@
 # src/ontology/propagation.py
+"""
+# CHANGE LOG (2026-01-07)
+# This function performs ancestor closure over the GO DAG.
+# In this project, it is used selectively:
+#
+# - Used for IC (Information Content) computation to obtain
+#   ontology-consistent term frequencies.
+# - NOT used during inference or submission, to avoid propagating
+#   incorrect predictions to higher-level GO terms.
+#
+# Optionally, ontology root terms (MF/BP/CC) can be dropped to stabilize
+# downstream statistics such as IC distributions.
+#
+# Cache stores parents only (excluding self); returned set includes self.
+
+"""
 from goatools.obo_parser import GODag
 
-_ancestor_cache = {}
+_ancestor_cache = {} # cache: go_id -> set(parents) excluding self
+GO_ROOTS = {"GO:0003674", "GO:0008150", "GO:0005575"}  # MF/BP/CC roots
 
-def propagate_ancestors(go_set, godag):
+def propagate_ancestors(go_set, godag,drop_roots: bool = False):
     """
     Expand a set of GO terms to include all ancestors
     using the GO DAG.
@@ -24,11 +41,13 @@ def propagate_ancestors(go_set, godag):
     for go_id in go_set:
         if go_id not in _ancestor_cache:
             if go_id in godag:
-                _ancestor_cache[go_id] = godag[go_id].get_all_parents()
+                _ancestor_cache[go_id] = set(godag[go_id].get_all_parents())
             else:
                 _ancestor_cache[go_id] = set()
 
         expanded |= _ancestor_cache[go_id]
+    if drop_roots:
+        expanded -= GO_ROOTS
 
 
     return expanded
